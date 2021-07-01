070fa26fc9a5b6cab66c02ef789412ca9660509b
fn-hide <85201868+fn-hide@users.noreply.github.com>
2021-07-01 13:39:02 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function keAboutMe() { 
+	document.getElementById("intro-section").scrollIntoView({ behavior: 'smooth' })
+}
+
+function keEducation() {
+	document.getElementById("riwayat-section").scrollIntoView({ behavior: 'smooth' })
+}
+
+function keSkills() {
+	document.getElementById("skill-section").scrollIntoView({ behavior: 'smooth' })
+	
+}
+
+function keContact() {
+	document.getElementById("contact-section").scrollIntoView({ behavior: 'smooth' })
+	
+}
+
+function submitForm() {
+	alert('Pesan berhasil terkirim');
+}
